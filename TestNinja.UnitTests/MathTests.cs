4cf82571988e400829ebeeca2c99a0b00b1c41d0
Kiril Spasov <spasov.kiril@gmail.com>
2023-02-02 12:33:41 +0200

    Added SetUp method to MathTests.

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -10,15 +10,19 @@ namespace TestNinja.UnitTests
     [TestFixture]
     internal class MathTests
     {
+        Fundamentals.Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Fundamentals.Math();
+        }
 
         [Test]
         public void Add_WhenCalled_ReturnSumOfArguments()
         {
-            //Arrange
-            Fundamentals.Math math = new Fundamentals.Math();
-
             //Act
-            int result = math.Add(1, 2);
+            int result = _math.Add(1, 2);
 
             //Assert
             Assert.That(result, Is.EqualTo(3));
@@ -27,11 +31,8 @@ namespace TestNinja.UnitTests
         [Test]
         public void Max_FirstArgumentIsGreater_ReturnFirstArgument()
         {
-            //Arrange
-            Fundamentals.Math math = new Fundamentals.Math();
-
             //Act
-            int result = math.Max(2, 1);
+            int result = _math.Max(2, 1);
 
             //Assert
             Assert.That(result, Is.EqualTo(2));
@@ -39,11 +40,8 @@ namespace TestNinja.UnitTests
         [Test]
         public void Max_SecondArgumentIsGreater_ReturnSecondArgument()
         {
-            //Arrange
-            Fundamentals.Math math = new Fundamentals.Math();
-
             //Act
-            int result = math.Max(1, 2);
+            int result = _math.Max(1, 2);
 
             //Assert
             Assert.That(result, Is.EqualTo(2));
@@ -51,11 +49,8 @@ namespace TestNinja.UnitTests
         [Test]
         public void Max_ArgumentsAreEqual_ReturnSameArgument()
         {
-            //Arrange
-            Fundamentals.Math math = new Fundamentals.Math();
-
             //Act
-            int result = math.Max(2, 2);
+            int result = _math.Max(2, 2);
 
             //Assert
             Assert.That(result, Is.EqualTo(2));
