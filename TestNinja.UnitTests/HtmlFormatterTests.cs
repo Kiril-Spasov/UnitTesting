309d22af17e9bb20c97e4b316256991b58dc790c
Kiril Spasov <spasov.kiril@gmail.com>
2023-02-02 13:23:42 +0200

    Added HtmlFormatterTests - testing strings.

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    internal class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithBoldElement()
+        {
+            //Arrange
+            HtmlFormatter formatter = new HtmlFormatter();
+
+            //Act
+            string result = formatter.FormatAsBold("test");
+
+            //Assert
+            Assert.That(result, Is.EqualTo("<strong>test</strong>"));
+        }
+    }
+}
