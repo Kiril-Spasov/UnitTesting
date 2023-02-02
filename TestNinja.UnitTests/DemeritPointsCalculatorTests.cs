813af70b5a5eb100fd8272634154007164a7686c
Kiril Spasov <spasov.kiril@gmail.com>
2023-02-02 18:04:54 +0200

    Testing CalculateDemeritPoints method.

---
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    internal class DemeritPointsCalculatorTests
+    {
+        DemeritPointsCalculator _pointsCalculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _pointsCalculator = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_IfSpeedIsNotValid_ThrowsArgumentOutOfRangeException(int speed)
+        {
+            Assert.That(() => _pointsCalculator.CalculateDemeritPoints(speed),
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnsDemeritPoints(int speed, int expectedResult)
+        {
+            int result = _pointsCalculator.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
