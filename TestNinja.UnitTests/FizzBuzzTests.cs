7083352a1b45fd7cf795f59e32eb624099f08a17
Kiril Spasov <spasov.kiril@gmail.com>
2023-02-02 17:30:17 +0200

    Testing GetOutput method on FizzBuzz class.

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    internal class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_WhenInputIsDivisibleBy3And5_ReturnsFizzBuzz()
+        {
+            string result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_WhenInputIsDivisibleBy3_ReturnsFizz()
+        {
+            string result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_WhenInputIsDivisibleBy5_ReturnsFizz()
+        {
+            string result = FizzBuzz.GetOutput(5);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsNotDivisibleBy3Or5_ReturnsInputToString()
+        {
+            string result = FizzBuzz.GetOutput(4);
+
+            Assert.That(result, Is.EqualTo("4"));
+        }
+    }
+}
