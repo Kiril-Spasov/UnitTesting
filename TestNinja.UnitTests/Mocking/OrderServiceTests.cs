9953de101823e4824a5903e1d7b3d053bee5d768
Kiril Spasov <spasov.kiril@gmail.com>
2023-02-06 13:11:48 +0200

    OrderServiceTests - testing interactions using Verify() method.

---
+++ TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    internal class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            Mock<IStorage> mock = new Mock<IStorage>();
+            OrderService service = new OrderService(mock.Object);
+            Order order = new Order();
+
+            service.PlaceOrder(order);
+
+            mock.Verify(s => s.Store(order));
+        }
+    }
+}
