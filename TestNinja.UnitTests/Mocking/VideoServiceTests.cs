46938cdf22b1b0c2a25a4d67ac41015ab26f248c
Kiril Spasov <spasov.kiril@gmail.com>
2023-02-06 12:57:11 +0200

    VideoServiceTests - creating mock objects using Moq.

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +11,22 @@ namespace TestNinja.UnitTests.Mocking
     [TestFixture]
     internal class VideoServiceTests
     {
+        private Mock<FileHandler> _mock;
+        private VideoService _video;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mock = new Mock<FileHandler>();
+            _video = new VideoService(_mock.Object);
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnsErrorMessage()
         {
-            VideoService video = new VideoService(new MockFileReader());
+            _mock.Setup(fr => fr.Read("video.txt")).Returns("");
 
-            string result = video.ReadVideoTitle();
+            string result = _video.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
